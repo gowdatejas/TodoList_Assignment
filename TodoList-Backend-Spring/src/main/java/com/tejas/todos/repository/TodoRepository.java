6059f50346d4c20ae1992e127472ef0a7d797796
tejas <tejascm@gmail.com>
2025-05-23 14:00:12 +0530

    Todo Assistant Project

---
+++ TodoList-Backend-Spring/src/main/java/com/tejas/todos/repository/TodoRepository.java
@@ -0,0 +1,13 @@
+package com.tejas.todos.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gowtham.todos.entity.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
+	List<Todo> findByCompletedFalse();
+
+}
---
+++ TodoList-Backend-Spring/src/test/java/com/tejas/todos/TodoListBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
